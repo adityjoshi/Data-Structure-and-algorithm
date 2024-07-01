5317dd0dfdb8c88293545a7fd420a615d0889c85
Aditya Joshi <aditya03joshi@gmail.com>
2024-07-01 15:34:29 +0530

    java-oops

--- src/code/oops/SingleInheritance.java
+++ src/code/oops/SingleInheritance.java
@@ -5,7 +5,12 @@ public class SingleInheritance {
         Executive obj = new Executive();
         System.out.println("salary credited " + obj.salary);
         System.out.println("Bonus of six month " + obj.bonus);
-
+        B obj2 = new B();
+        obj2.a = 10;
+        obj2.b=20;
+        obj2.c=30;
+        obj2.display();
+        obj2.show();
     }
 }
 
@@ -14,4 +19,18 @@ class Employ {
 }
 class Executive extends Employ {
     float bonus = 300 * 6;
+}
+
+class A {
+    int a,b;
+    void display() {
+        System.out.println("Inside class A values = "+a+" "+b);
+    }
+}
+
+class B extends A {
+    int c;
+    void show() {
+        System.out.println("Inside B="+a+" "+b+" "+c);
+    }
 }
