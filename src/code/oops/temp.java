d465e2b2456fc76e20c3a665b5c8f5b915f58661
Aditya Joshi <aditya03joshi@gmail.com>
2024-07-10 15:04:46 +0530

    java

---
+++ src/code/oops/temp.java
@@ -0,0 +1,15 @@
+package code.oops;
+
+import java.util.Scanner;
+
+public class temp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        int x = in.nextInt();
+        System.out.println("Enter 2nd number");
+        int y = in.nextInt();
+        System.out.println("Sum of two number is ");
+        System.out.println(x+y);
+    }
+        }
