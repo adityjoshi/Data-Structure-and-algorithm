45c8b08dd9beed6cb744c8e20315a089ea65ba52
Aditya Joshi <aditya03joshi@gmail.com>
2024-07-17 18:43:45 +0530

    java

---
+++ src/code/oops/assignment/p13.java
@@ -0,0 +1,12 @@
+package code.oops.assignment;
+
+public class p13 {
+    public static void main(String[] args) {
+        int x = 5;
+        int ans = 1;
+        for (int i = x ; i>=1; i--) {
+            ans *=i;
+        }
+        System.out.println(ans);
+    }
+}
