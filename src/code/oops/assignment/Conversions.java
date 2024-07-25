d09134f35f304a91118c1aaa6b4ff1fa2c218fe2
Aditya Joshi <aditya03joshi@gmail.com>
2024-07-25 15:29:18 +0530

    java

---
+++ src/code/oops/assignment/Conversions.java
@@ -0,0 +1,23 @@
+package code.oops.assignment;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Conversions {
+    public static void main(String[] args) {
+        String x = "apple" ;
+        String y = "banana" ;
+        String z = "grapes" ;
+        String UpperCase = "X in upper case : \n"+ x.toUpperCase()  + "\n y in upper case : \n" + y.toUpperCase() + "\n z in upper case : \n" + z.toUpperCase();
+        System.out.println(UpperCase);
+        int index = x.indexOf('l');
+        System.out.println("index of the word l is  "+ index);
+        String data = "apple,banana,mango";
+        String [] fruitArray = data.split(",");
+        System.out.println(Arrays.toString(fruitArray));
+
+        String rep = "Hello, dev";
+        String ans =  rep.replace("dev","golang");
+        System.out.println(ans);
+    }
+}
