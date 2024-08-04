61d6206511cefe4017792fdcf62468b8d3b3ba36
Aditya Joshi <aditya03joshi@gmail.com>
2024-08-04 23:06:15 +0530

    java

--- src/code/DS/Hashmaps/Sets.java
+++ src/code/DS/Hashmaps/Sets.java
@@ -1,7 +1,14 @@
 package code.DS.Hashmaps;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Sets {
     public static void main(String[] args) {
-
+        Set<String> x = new HashSet<>();
+        x.add("adi");
+        x.add("nisarg");
+        x.add("ghar");
+        System.out.println(x.contains("ghar"));
     }
 }
