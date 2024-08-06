19f0d552eac072bf9f983a4aedf3270df0f79f21
Aditya Joshi <aditya03joshi@gmail.com>
2024-08-06 23:22:11 +0530

    java

--- src/code/DS/NC/AandH/ProductOfArrayDiscludingSelf.java
+++ src/code/DS/NC/AandH/ProductOfArrayDiscludingSelf.java
@@ -1,11 +1,32 @@
 package code.DS.NC.AandH;
 
+import java.util.Arrays;
+
 public class ProductOfArrayDiscludingSelf {
     public static void main(String[] args) {
         int [] arr = {1,2,3,4};
-        int [] result = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            result= i+1
+        System.out.println(Arrays.toString(productExceptSelf(arr)));
+    }
+
+        static  int[] productExceptSelf(int[] nums) {
+            int [] left = new int [nums.length];
+            int [] right = new int [nums.length];
+
+            left[0] = 1;
+            right[nums.length - 1] = 1;
+
+            for (int i = 1; i<nums.length ; i++) {
+                left[i] = left[i-1] * nums[i-1];
+            }
+            for (int i = nums.length-2; i>-1; i--) {
+                right[i] = right[i+1] * nums[i+1];
+            }
+            int [] ans = new int[nums.length];
+            for (int i = 0; i<nums.length;i++) {
+                ans[i] = left[i]*right[i];
+            }
+            return ans ;
         }
     }
-}
+
+
