9ed1a1a9c2e59e54bc30142f309a5ca05de66b2e
Aditya Joshi <aditya03joshi@gmail.com>
2024-08-15 12:20:53 +0530

    java

---
+++ src/code/oops/assignment/ass_3/p6.java
@@ -0,0 +1,28 @@
+package code.oops.assignment.ass_3;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class p6 {
+    public static void main(String[] args) {
+        String inp = "source.txt";
+        String out = "outputFile.txt";
+
+        try (FileReader fileReader = new FileReader(inp);
+             FileWriter fileWriter = new FileWriter(out)
+        ) {
+            int character;
+            // Read characters from the input file and write them to the output file
+            while ((character = fileReader.read()) != -1) {
+                fileWriter.write(character);
+            }
+
+            System.out.println("File copied successfully.");
+
+        } catch (IOException e) {
+            System.out.println("error"+ e.getMessage());
+        }
+    }
+}
