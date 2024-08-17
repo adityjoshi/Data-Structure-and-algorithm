8c947b18dc722735036985b7624b155df3a0acf6
Aditya Joshi <aditya03joshi@gmail.com>
2024-08-17 11:23:00 +0530

    java

---
+++ src/code/oops/assignment/ass_3/p8.java
@@ -0,0 +1,29 @@
+package code.oops.assignment.ass_3;
+
+import java.io.*;
+import java.nio.Buffer;
+
+public class p8 {
+    public static void main(String[] args) {
+        String inp = "source.txt";
+        String out = "outputFile.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inp))) {
+            String line ;
+            while((line = reader.readLine())!=null) {
+                System.out.println("read line => " + line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(out))) {
+            writer.write("hello word");
+            writer.newLine();
+            writer.write("billo tu agg hai");
+            writer.newLine();
+            System.out.println("data has been written" + out);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
