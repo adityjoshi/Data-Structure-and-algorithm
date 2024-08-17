059057b1e86168c00e5756f6e930a784e03b4673
Aditya Joshi <aditya03joshi@gmail.com>
2024-08-17 12:26:43 +0530

    java

---
+++ src/code/oops/assignment/ass_3/p9.java
@@ -0,0 +1,30 @@
+package code.oops.assignment.ass_3;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class p9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter data");
+        String file = in.nextLine();
+
+        File outputFile = new File(file);
+
+        try (PrintWriter writer = new PrintWriter(outputFile)) {
+            System.out.println("enter the data and exit to stop");
+            String input = in.nextLine();
+            while(!false) {
+                if("exit".equalsIgnoreCase(input));
+                break;
+            }
+            writer.println(input);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            in.close();
+        }
+
+    }
+}
