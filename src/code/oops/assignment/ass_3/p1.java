28e7f1f98ef828fcb661ea59213fded6b5e19aaa
Aditya Joshi <aditya03joshi@gmail.com>
2024-08-18 15:55:14 +0530

    java

--- src/code/oops/assignment/ass_3/p1.java
+++ src/code/oops/assignment/ass_3/p1.java
@@ -3,7 +3,7 @@ package code.oops.assignment.ass_3;
 public class p1 {
     public static void main(String[] args) {
         System.out.println(divide(2,0));
-        System.out.println(divide(0,0));
+        System.out.println(divide(0,1));
     }
     static Integer divide(int n, int d) {
         try {
