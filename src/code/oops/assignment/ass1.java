4569b98bdee77154a0a3c991760ec5d92e329f3d
Aditya Joshi <aditya03joshi@gmail.com>
2024-08-20 10:24:33 +0530

    java

---
+++ src/code/oops/assignment/ass1.java
@@ -0,0 +1,12 @@
+package code.oops.assignment;
+
+public class ass1 {
+    public static void main(String[] args) {
+        int num = 5 ;
+        int ans = 1;
+        for (int i = 1; i <= num; i++) {
+           ans = ans * i ;
+        }
+        System.out.println(ans);
+    }
+}
